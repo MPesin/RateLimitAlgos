e6f2be4ef839e621ff46788c8aecd4b2ac7dbc7f
MPesin <mikelpesin@gmail.com>
2024-01-24 19:05:07 +0200

    passing time windows test resetting totals

--- DosAssignment/RateLimiters/StaticRateLimiter.cs
+++ DosAssignment/RateLimiters/StaticRateLimiter.cs
@@ -1,26 +1,55 @@
+using System.Timers;
 using DosAssignment.RateLimiters.Exceptions;
+using Timer = System.Timers.Timer;
 
 namespace DosAssignment.RateLimiters;
 
 public class StaticRateLimiter : IRateLimiter
 {
-    private readonly int _totalRequests;
-    private readonly TimeSpan _timeWindow;
+    private readonly int _maxRequests;
+    private readonly Timer _windowTimer;
+    private readonly AutoResetEvent _resetEvent = new(true);
 
-    public StaticRateLimiter(int totalRequests, TimeSpan timeWindow)
+    public StaticRateLimiter(int maxRequests, TimeSpan timeWindow)
     {
-        _totalRequests = totalRequests;
-        _timeWindow = timeWindow;
+        _maxRequests = maxRequests;
+        _windowTimer = new Timer(timeWindow);
+        _windowTimer.Elapsed += OnWindowTimerElapsed;
+    }
+
+    private void OnWindowTimerElapsed(object? sender, ElapsedEventArgs e)
+    {
+        _resetEvent.WaitOne();
+        TotalRequestsSet = 0;
+        _resetEvent.Set();
     }
 
     public int TotalRequestsSet { get; private set; }
 
     public Task SetRequestAsync()
     {
-        var nextValue = TotalRequestsSet + 1;
-        TotalRequestsSet = nextValue <= _totalRequests
-            ? nextValue
-            : throw new RequestLimitReachedException();
+        try
+        {
+            _resetEvent.WaitOne();
+            HandleFirstRequestInWindow();
+            var nextValue = TotalRequestsSet + 1;
+            TotalRequestsSet = nextValue <= _maxRequests
+                ? nextValue
+                : throw new RequestLimitReachedException();
+        }
+        finally
+        {
+            _resetEvent.Set();
+        }
+
         return Task.CompletedTask;
+    }
+
+    private void HandleFirstRequestInWindow()
+    {
+        if (TotalRequestsSet == 0)
+        {
+            _windowTimer.Start();
+        }
     }
 }
