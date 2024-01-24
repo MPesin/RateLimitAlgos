aab4bf4593d6fe8df7b7ce61099a51bd9f335851
MPesin <mikelpesin@gmail.com>
2024-01-25 00:03:35 +0200

    implementing sliding window log algorithm

--- DosAssignment/RateLimiters/SlidingRateLimiter.cs
+++ DosAssignment/RateLimiters/SlidingRateLimiter.cs
@@ -1,11 +1,48 @@
+using DosAssignment.RateLimiters.Exceptions;
+
 namespace DosAssignment.RateLimiters;
 
 public class SlidingRateLimiter : IRateLimiter
 {
-    public Task SetRequestAsync()
+    private readonly int _maxRequests;
+    private readonly TimeSpan _timeWindow;
+    private readonly Queue<DateTime> _requestsTimestampQueue;
+
+    public SlidingRateLimiter(int maxRequests, TimeSpan timeWindow)
     {
-        throw new NotImplementedException();
+        _requestsTimestampQueue = new Queue<DateTime>();
+        _maxRequests = maxRequests;
+        _timeWindow = timeWindow;
     }
 
-    public int TotalRequestsSet { get; }
+    public Task SetRequestAsync()
+    {
+        var now = DateTime.Now;
+        ClearQueueFromOldTimestamps(now);
+        _requestsTimestampQueue.Enqueue(now);
+        if (_requestsTimestampQueue.Count >= _maxRequests)
+        {
+            throw new RequestLimitReachedException();
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private void ClearQueueFromOldTimestamps(DateTime now)
+    {
+        while (_requestsTimestampQueue.Any()
+               && IsNextTimestampNotInWindow(now))
+        {
+            _requestsTimestampQueue.Dequeue();
+        }
+    }
+
+    private bool IsNextTimestampNotInWindow(DateTime now)
+    {
+        var nextTimestamp = _requestsTimestampQueue.Peek();
+        var timePassed = now.Subtract(nextTimestamp);
+        return timePassed.CompareTo(_timeWindow) > 0;
+    }
+
+    public int TotalRequestsSet => _requestsTimestampQueue.Count;
 }
