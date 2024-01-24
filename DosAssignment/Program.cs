7d6f8c1670e52ef3255914b94bf1d0429792cc80
MPesin <mikelpesin@gmail.com>
2024-01-25 01:30:29 +0200

    configuring at startup

--- DosAssignment/Program.cs
+++ DosAssignment/Program.cs
@@ -1,9 +1,9 @@
+using DosAssignment.RateLimiters.Managers;
+
+RateLimiterService.Configure(new RateLimitConfig(5, TimeSpan.FromSeconds(5)));
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
